63e80fba877d5cff6daa33b9971d668b96570d14
Mohamedd Magdy <mohamedmagdyelsayed7@gmail.com>
2024-10-08 16:51:41 +0300

    Add Work section

--- scripts/global.js
+++ scripts/global.js
@@ -1,5 +1,16 @@
 let mobileMenuBtn = document.querySelector(".mobile-menu");
 let navigation = document.querySelector("nav");
-mobileMenuBtn.addEventListener("click",()=> {
-  navigation.classList.toggle("show-me")
-})
+mobileMenuBtn.addEventListener("click", () => {
+  navigation.classList.toggle("show-me");
+});
+
+// Remove Show-me Class From Navigation When Click Any spot
+document.onclick = function (e) {
+  console.log(e.target);
+  if (
+    !e.target.classList.contains("fa-bars") &&
+    !e.target.classList.contains("mobile-menu")
+  ) {
+    navigation.classList.remove("show-me");
+  }
+};
